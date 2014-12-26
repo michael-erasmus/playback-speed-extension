7b73b3945051eaeae2d97930a1e6d92e38951c90
Michael Erasmus <michaelerasmus@gmail.com>
2014-12-27 00:31:45 +0200

    Actually set the video speed

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,14 +1,26 @@
 'use strict';
 
+var setSpeed = function (speed) {
+  Array.prototype.forEach.call(document.querySelectorAll('video'),function (v) {
+    v.playbackRate = speed;
+  });
+};
+
+var setSpeedLabel = function(speed) {
+    var label = document.querySelectorAll('#speed-label');
+
+    Array.prototype.forEach.call(label, function(lbl){
+      lbl.innerHTML = speed + 'x';
+    });
+};
+
 var elements = document.querySelectorAll('.speed');
 
 Array.prototype.forEach.call(elements, function(el){
-    el.onchange = function () {
-        var label = document.querySelectorAll('#speed-label');
-
-        Array.prototype.forEach.call(label, function(lbl){
-            lbl.innerHTML = el.value + 'x';
-        });
-    };
-    el.oninput = el.onchange;
+  el.onchange = function () {
+    var code = '(' + setSpeed.toString() + '(' + el.value +' ))';
+    chrome.tabs.executeScript(null, {code:code, allFrames: true});
+    setSpeedLabel(el.value);
+  };
+  el.oninput = el.onchange;
 });
