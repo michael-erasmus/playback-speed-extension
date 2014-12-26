2ec5eb0e6249e59616ee0909bf11539a9b0f3bcc
Michael Erasmus <michaelerasmus@gmail.com>
2014-12-27 00:53:34 +0200

    Grab the current speed when opening the popup

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,5 +1,12 @@
 'use strict';
 
+var getSpeed = function () {
+  var speed = 0;
+  Array.prototype.forEach.call(document.querySelectorAll('video'),function (v) {
+    speed = v.playbackRate;
+  });
+  return speed;
+};
 var setSpeed = function (speed) {
   Array.prototype.forEach.call(document.querySelectorAll('video'),function (v) {
     v.playbackRate = speed;
@@ -7,19 +14,29 @@ var setSpeed = function (speed) {
 };
 
 var setSpeedLabel = function(speed) {
-    var label = document.querySelectorAll('#speed-label');
+  var label = document.querySelectorAll('#speed-label');
 
-    Array.prototype.forEach.call(label, function(lbl){
-      lbl.innerHTML = speed + 'x';
-    });
+  Array.prototype.forEach.call(label, function(lbl){
+    lbl.innerHTML = speed + 'x';
+  });
 };
 
 var elements = document.querySelectorAll('.speed');
 
 Array.prototype.forEach.call(elements, function(el){
+  //grab the current speed
+  var getSpeedCode = '(' + getSpeed.toString() + '())';
+  chrome.tabs.executeScript(null, {code:getSpeedCode, allFrames: true}, function(response){
+    for(var o in response) {
+      if(response[o] > 0) {
+        setSpeedLabel(response[o]);
+      }
+    }
+  });
+
   el.onchange = function () {
-    var code = '(' + setSpeed.toString() + '(' + el.value +' ))';
-    chrome.tabs.executeScript(null, {code:code, allFrames: true});
+    var setSpeedCode = '(' + setSpeed.toString() + '(' + el.value +' ))';
+    chrome.tabs.executeScript(null, {code:setSpeedCode, allFrames: true});
     setSpeedLabel(el.value);
   };
   el.oninput = el.onchange;
